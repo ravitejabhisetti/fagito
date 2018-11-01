dfdd8a480686e101064411a79d9e7fbfd1610e59
e5558005 <ravi.bhisetti@fisglobal.com>
2018-11-01 18:55:33 +0530

    commented code for hiding the splash screen have to implement splash screen in android devices uncomment 2 lines one in component and another in xcode for splash screen to work in IOS devices

--- android/app/src/main/java/com/fagito/MainApplication.java
+++ android/app/src/main/java/com/fagito/MainApplication.java
@@ -9,39 +9,41 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.reactnativenavigation.react.ReactGateway;
+
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication {
+   
+   @Override
+   protected ReactGateway createReactGateway() {
+       ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+           @Override
+           protected String getJSMainModuleName() {
+               return "index";
+           }
+       };
+       return new ReactGateway(this, isDebug(), host);
+   }
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+   @Override
+   public boolean isDebug() {
+       return BuildConfig.DEBUG;
+   }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new SplashScreenReactPackage()
-      );
-    }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+   protected List<ReactPackage> getPackages() {
+       // Add additional packages you require here
+       // No need to add RnnPackage and MainReactPackage
+       return Arrays.<ReactPackage>asList(
+           // eg. new VectorIconsPackage()
+       );
+   }
+ 
+   @Override
+   public List<ReactPackage> createAdditionalReactPackages() {
+       return getPackages();
+   }
 }
