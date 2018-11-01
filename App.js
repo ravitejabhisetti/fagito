404a4e0002bf83a9495b6f6ed80d41289f591b24
e5558005 <ravi.bhisetti@fisglobal.com>
2018-11-01 13:51:37 +0530

    reverted back the splash screen changes: Splash screen is not there in Android devices: Have to implement splash screen in Android devices

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native</Text>
+        <Text style={styles.welcome}>Welcome to React Native course</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
