924ca6f0739ceb0183c8f37167c7d74f26a9cf9f
e5558005 <ravi.bhisetti@fisglobal.com>
2018-12-30 11:37:23 +0530

    added component for modal

---
+++ src/components/fagito-modal/fagito-modal.component.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { STYLES } from './fagito-modal.style';
+
+class FagitoModalComponent extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <View>
+                <Text>Fagito Modal Component</Text>
+            </View>
+        )
+    }
+}
+
+export default FagitoModalComponent;
