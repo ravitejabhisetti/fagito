f4e5d702183db6b073c588e1cbd96b25687c082a
e5558005 <ravi.bhisetti@fisglobal.com>
2018-12-31 08:16:00 +0530

    updated todo

--- todo.js
+++ todo.js
@@ -1,4 +1,5 @@
 /**
  *  add shadow for the border bottom unders the dates for delivery component
  *  in reset password on click of reset password show the necessary message in the alert
+ *  add ripple effect for the buttons
  */
