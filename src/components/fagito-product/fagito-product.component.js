fc7e92b24d66d36af84044ed6c8522c589af8911
e5558005 <ravi.bhisetti@fisglobal.com>
2019-01-03 22:43:14 +0530

    added default image to the products

--- src/components/fagito-product/fagito-product.component.js
+++ src/components/fagito-product/fagito-product.component.js
@@ -5,12 +5,20 @@ import { STYLES } from './fagito-product.style';
 class FagitoProduct extends Component {
     constructor(props) {
         super(props);
+        this.state = { showDefault: true, error: false };
     }
     render() {
+        let isProductVeg = (this.props.product.base.isVeg ? require('../../assets/veg.jpg') : require('../../assets/nonveg.jpg'));
+        let image = (this.state.showDefault || !this.props.product.image) ? require('../../assets/dummyFoodImage.jpg') :
+            ((this.state.error || !this.props.product.image) ? require('../../assets/dummyFoodImage.jpg') : { uri: this.props.product.image });
         return (
             <View style={STYLES.productSegment}>
-                {/* <Text>{this.props.product.base.name}</Text> */}
-                <Image style={STYLES.image} source={{ uri: this.props.product.image }}></Image>
+                <Image style={STYLES.image}
+                    onLoadEnd={() => this.setState({ showDefault: false })}
+                    onError={() => this.setState({ error: true })}
+                    source={image}>
+                </Image>
+                <Text style={STYLES.productName}><Image style={STYLES.icon} source={isProductVeg}></Image> {this.props.product.base.name}</Text>
             </View>
         )
     }
--- src/components/fagito-product/fagito-product.style.js
+++ src/components/fagito-product/fagito-product.style.js
@@ -3,13 +3,34 @@ import * as style from '../../common/fagito-style-constants';
 
 const STYLES = StyleSheet.create({
     productSegment: {
-        backgroundColor: 'red',
-        flexBasis: '33%'
+        backgroundColor: style.FAGITO_WHITE_COLOR,
+        flexBasis: '33%',
+        paddingTop: 5,
+        paddingRight: 5,
+        paddingLeft: 5,
+        paddingBottom: 0
     },
     image: {
         width: '100%',
-        minHeight: 100,
+        height: 100,
         resizeMode: 'cover'
+    },
+    productName: {
+        fontSize: 12,
+        fontFamily: style.FAGITO_FONT_FAMILY_LATO,
+        color: style.FAGITO_BLACK_COLOR,
+        overflow: style.HIDDEN,
+        height: 45,
+        lineHeight: 13,
+        marginTop: 4,
+        marginRight: 0,
+        marginBottom: 2,
+        marginLeft: 0
+    },
+    icon: {
+        height: 10,
+        width: 10,
+        fontSize: 12
     }
 });
 
