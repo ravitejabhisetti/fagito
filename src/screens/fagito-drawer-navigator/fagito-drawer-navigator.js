6494103068e0e4dd1fba0eeda711a38695e39f20
e5558005 <ravi.bhisetti@fisglobal.com>
2019-01-05 10:09:50 +0530

    updated the name in the drawer component

--- src/screens/fagito-drawer-navigator/fagito-drawer-navigator.js
+++ src/screens/fagito-drawer-navigator/fagito-drawer-navigator.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import { SafeAreaView, ScrollView, Dimensions, View, Text } from 'react-native';
+import { SafeAreaView, ScrollView, Dimensions, View, Text, AsyncStorage } from 'react-native';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation';
 import {
     FagitoHomeScreen, FagitoWalletScreen, FagitoMealPassScreen,
     FagitoFeedbackScreen, FagitoHistoryScreen, FagitoOffersScreen,
     FagitoSupportScreen, FagitoSettingsScreen, FagitoFreeFoodScreen
 } from '../fagito-screens';
-import { FAGITO_HOME_SCREEN, DRAWER_POSITION_LEFT } from '../../common/fagito-constants';
+import { FAGITO_HOME_SCREEN, DRAWER_POSITION_LEFT, FAGITO_USER_DETAILS } from '../../common/fagito-constants';
 import * as style from '../../common/fagito-style-constants';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { STYLES } from './fagito-drawer-navigator.style';
 
-// const CustomDrawerComponent = (props) => (
 class CustomDrawerComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { userDetails: null };
+    }
+    componentDidMount() {
+        AsyncStorage.getItem(FAGITO_USER_DETAILS).then(userDetails => {
+            this.setState((state) => {
+                return {
+                    ...state,
+                    userDetails: JSON.parse(userDetails)
+                }
+            })
+        })
+    }
     render() {
         return (
             <SafeAreaView style={STYLES.safeArea}>
                 <View style={STYLES.headerSegment}>
                     <View style={STYLES.headerTitle}>
                         <Icon name='md-person' color='white' size={25} />
-                        <Text style={STYLES.headerText}>Ravi Teja Bhisetti</Text>
+                        <Text style={STYLES.headerText}>{this.state.userDetails && this.state.userDetails.name}</Text>
                     </View>
                 </View>
                 <ScrollView>
