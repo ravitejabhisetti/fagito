d2986cfce5928d2dc85b1b0351d0b87f8eb439e2
e5558005 <ravi.bhisetti@fisglobal.com>
2019-01-27 21:18:38 +0530

    added top bar navigator in history screen

--- src/screens/fagito-history-screen/fagito-history-screen.js
+++ src/screens/fagito-history-screen/fagito-history-screen.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text } from 'react-native';
 import { BackIcon, FagitoOrdersComponent, FagitoPaymentsComponent, FagitoTransactionsComponent } from '../../components/fagito-components';
 import { HISTORY_TITLE, FAGITO_HOME_SCREEN } from '../../common/fagito-constants';
 import * as style from '../../common/fagito-style-constants';
 import { STYLES } from './fagito-history-screen-style';
+import { createBottomTabNavigator, createMaterialTopTabNavigator, createAppContainer } from 'react-navigation';
 
 class FagitoHistoryScreen extends Component {
     render() {
@@ -11,15 +12,16 @@ class FagitoHistoryScreen extends Component {
             <View style={STYLES.historySection}>
                 <BackIcon iconColor={style.FAGITO_WHITE_COLOR} navigateToHome={() => this.props.navigation.navigate(FAGITO_HOME_SCREEN)}
                     backgroundColor={style.FAGITO_BUTTON_COLOR} title={HISTORY_TITLE} />
-                <ScrollView>
-                    <Text>check the transac history screen</Text>
-                    <FagitoOrdersComponent></FagitoOrdersComponent>
-                    <FagitoPaymentsComponent></FagitoPaymentsComponent>
-                    <FagitoTransactionsComponent></FagitoTransactionsComponent>
-                </ScrollView>
+                    <TransactionsTabNavigator></TransactionsTabNavigator>
             </View>
         )
     }
 }
+
+const TransactionsTabNavigator = createMaterialTopTabNavigator({
+    ALL: FagitoTransactionsComponent,
+    PAYMENTS: FagitoPaymentsComponent,
+    ORDERS: FagitoOrdersComponent
+})
 
 export default FagitoHistoryScreen;
